50a4db27551ec9f3ec6af511ea7901925ab1f52c
Joe Orchard <JO278870@falmouth.ac.uk>
2025-06-24 17:38:54 +0100

    Got started on player controller

--- Assets/Scripts/SCR_PlayerController.cs
+++ Assets/Scripts/SCR_PlayerController.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class SCR_PlayerController : MonoBehaviour
 {
-    private 
+    private Rigidbody2D playerRB;
+    [SerializeField] float gravity = -9.81f;
+    public float moveSpeed = 5f;
+    public float runSpeed = 12f;
+    public float jumpHeight = 2f;
+    private Vector3 velocity;
+    private float ySpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
     }
 }
