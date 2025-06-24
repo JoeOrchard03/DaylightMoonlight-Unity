c6161942dbdd3492cbeac606eb4347f06674937a
Joe Orchard <JO278870@falmouth.ac.uk>
2025-06-24 20:18:17 +0100

    got basic jump working

--- Assets/Scripts/SCR_PlayerController.cs
+++ Assets/Scripts/SCR_PlayerController.cs
@@ -5,27 +5,54 @@ using UnityEngine.Scripting.APIUpdating;
 
 public class SCR_PlayerController : MonoBehaviour
 {
-    private Rigidbody2D playerRB;
-    [SerializeField] float gravity = -9.81f;
-    public float moveSpeed = 5f;
-    public float runSpeed = 12f;
-    public float jumpHeight = 2f;
-    private Vector3 velocity;
-    private float ySpeed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        playerRB = GetComponent<Rigidbody2D>();
-    }
+    public Rigidbody2D playerRB;
+    public float groundRayCastDistance = 1.25f;
+    public LayerMask groundLayerMask;
+    public float walkSpeed = 5f;
+    public float sprintSpeed = 12f;
+    public bool isGrounded = false;
+    public float jumpForce = 100f;
 
     // Update is called once per frame
     void Update()
     {
+        GroundedCheck();
         Move();
+    }
+    
+    private void GroundedCheck()
+    {
+        Vector2 down = transform.TransformDirection(Vector2.down) * groundRayCastDistance;
+        Debug.DrawRay(transform.position, down);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundRayCastDistance, groundLayerMask);
+        if(hit)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
 
     private void Move()
     {
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRB.velocity = (new Vector2(playerRB.velocity.x, jumpForce));
+            isGrounded = false;
+        }
+
+        float XInput = Input.GetAxis("Horizontal");
+
+        if(Mathf.Abs(XInput) > 0)
+        {
+            playerRB.velocity = new Vector2(XInput * walkSpeed, playerRB.velocity.y);
+        }
+    }
+
+    private void Jump()
+    {
+        
     }
 }
