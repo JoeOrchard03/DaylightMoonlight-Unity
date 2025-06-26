32ea692c9db83cde5a950a61b5d7ee81cad1df3a
Joe Orchard <JO278870@falmouth.ac.uk>
2025-06-26 15:18:30 +0100

    added variable jump height based on held time

--- Assets/Scripts/SCR_PlayerController.cs
+++ Assets/Scripts/SCR_PlayerController.cs
@@ -1,23 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
 
 public class SCR_PlayerController : MonoBehaviour
 {
-    public Rigidbody2D playerRB;
-    public float groundRayCastDistance = 1.25f;
-    public LayerMask groundLayerMask;
+    [Header("Movement values")]
     public float walkSpeed = 5f;
     public float sprintSpeed = 12f;
-    public bool isGrounded = false;
-    public float jumpForce = 100f;
+    private float moveSpeed;
 
-    // Update is called once per frame
+    [Header("Jump values")]
+    public float initialJumpForce;
+    public float heldJumpForce;
+    public float maxJumpDuration = 0.3f;
+    private bool jumpCanContinue = true;
+    private float jumpTimer = 0.0f;
+    private bool isJumping = false;
+
+    [Header("Key Binds")]
+    public KeyCode sprintButton;
+    public KeyCode jumpButton;
+
+    [Header("Objects")]
+    public Rigidbody2D playerRB;
+
+    [Header("Ground check variables")]
+    public float groundRayCastDistance = 1.25f;
+    public LayerMask groundLayerMask;
+    public bool isGrounded = false;
+
+    [Header("Misc variables")]
+    private float startTime;
+
+    private void Start()
+    {
+        moveSpeed = walkSpeed;
+    }
+
     void Update()
     {
         GroundedCheck();
+        JumpCheck();
         Move();
+        SprintCheck();
     }
     
     private void GroundedCheck()
@@ -37,22 +65,57 @@ public class SCR_PlayerController : MonoBehaviour
 
     private void Move()
     {
-        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
-        {
-            playerRB.velocity = (new Vector2(playerRB.velocity.x, jumpForce));
-            isGrounded = false;
-        }
-
         float XInput = Input.GetAxis("Horizontal");
 
-        if(Mathf.Abs(XInput) > 0)
+        if (Mathf.Abs(XInput) > 0)
         {
-            playerRB.velocity = new Vector2(XInput * walkSpeed, playerRB.velocity.y);
+            playerRB.velocity = new Vector2(XInput * moveSpeed, playerRB.velocity.y);
         }
     }
 
-    private void Jump()
+    private void JumpCheck()
     {
-        
+        if (isGrounded && Input.GetKeyDown(jumpButton))
+        {
+            playerRB.AddForce(transform.up * initialJumpForce, ForceMode2D.Impulse);
+            isJumping = true;
+            jumpCanContinue = true;
+            jumpTimer = 0.0f;
+        }
+
+        if (isJumping && Input.GetKey(jumpButton) && jumpCanContinue)
+        {
+            jumpTimer += Time.deltaTime;
+            if (jumpTimer < maxJumpDuration)
+            {
+                playerRB.AddForce(transform.up * heldJumpForce, ForceMode2D.Force);
+            }
+            else
+            {
+                jumpCanContinue = false;
+            } 
+        }
+
+        if(Input.GetKeyUp(jumpButton))
+        {
+            jumpCanContinue = false;
+        }
+
+        if(isGrounded && !Input.GetKey(jumpButton))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void SprintCheck()
+    {
+        if(Input.GetKey(sprintButton) && isGrounded)
+        {
+            moveSpeed = sprintSpeed;
+        }
+        if(!Input.GetKey(sprintButton) && isGrounded)
+        {
+            moveSpeed = walkSpeed;
+        }
     }
 }
